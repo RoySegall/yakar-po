8392400ad542eca6696ddee509eebd0d88546fee
Roy Segall <roy@segall.io>
2019-06-16 22:46:26 +0300

    Adding more stuff to the product page.

--- front/testing/src/mockups/products.js
+++ front/testing/src/mockups/products.js
@@ -1,6 +1,7 @@
 export default {
   "6": {
     id: 6,
+    average_price: "5 nis",
     image: "http://www.office-exp.co.il/ProductsImages/L357934.jpg",
     name: "Milk",
     hi_price: {
@@ -10,10 +11,43 @@ export default {
     low_price: {
       amount: "3 nis",
       location: "Shufersal"
-    }
+    },
+    samples: [
+      {
+        brand: "Rami Levy",
+        branch: "Ramat gan - sheset haymim 152",
+        price: "3.4",
+        sampled_at: "Today, 14:52",
+      },
+      {
+        brand: "Shufersal",
+        branch: "Ramat gan - sheset haymim 15",
+        price: "3.2",
+        sampled_at: "Today, 14:42",
+      },
+      {
+        brand: "Yenot bitan",
+        branch: "Tel Aviv - zabotinsky 3",
+        price: "3.0",
+        sampled_at: "Today, 14:42",
+      },
+      {
+        brand: "Hatzi Hinam",
+        branch: "Beer Sheva - Main street 10",
+        price: "3.1",
+        sampled_at: "Today, 14:44",
+      },
+      {
+        brand: "Mega",
+        branch: "Haifa - Sheset yamim 20",
+        price: "4.1",
+        sampled_at: "Today, 14:35",
+      },
+    ],
   },
   "7": {
     id: 7,
+    average_price: "4 nis",
     image:
       "https://www.market-p.co.il/wp-content/uploads/2018/03/Geemaps.co_.il-36-4.jpg",
     name: "Butter",
@@ -24,10 +58,43 @@ export default {
     low_price: {
       amount: "2 nis",
       location: "Shufersal"
-    }
+    },
+    samples: [
+      {
+        brand: "Rami Levy",
+        branch: "Ramat gan - sheset haymim 152",
+        price: "3.4",
+        sampled_at: "Today, 14:52",
+      },
+      {
+        brand: "Shufersal",
+        branch: "Ramat gan - sheset haymim 15",
+        price: "3.2",
+        sampled_at: "Today, 14:42",
+      },
+      {
+        brand: "Yenot bitan",
+        branch: "Tel Aviv - zabotinsky 3",
+        price: "3.0",
+        sampled_at: "Today, 14:42",
+      },
+      {
+        brand: "Hatzi Hinam",
+        branch: "Beer Sheva - Main street 10",
+        price: "3.1",
+        sampled_at: "Today, 14:44",
+      },
+      {
+        brand: "Mega",
+        branch: "Haifa - Sheset yamim 20",
+        price: "4.1",
+        sampled_at: "Today, 14:35",
+      },
+    ],
   },
   "8": {
     id: 8,
+    average_price: "7 nis",
     image:
       "https://www.market-p.co.il/wp-content/uploads/2017/07/coca-cola.jpg",
     name: "Coca cola",
@@ -38,6 +105,38 @@ export default {
     low_price: {
       amount: "9 nis",
       location: "Shufersal"
-    }
+    },
+    samples: [
+      {
+        brand: "Rami Levy",
+        branch: "Ramat gan - sheset haymim 152",
+        price: "3.4",
+        sampled_at: "Today, 14:52",
+      },
+      {
+        brand: "Shufersal",
+        branch: "Ramat gan - sheset haymim 15",
+        price: "3.2",
+        sampled_at: "Today, 14:42",
+      },
+      {
+        brand: "Yenot bitan",
+        branch: "Tel Aviv - zabotinsky 3",
+        price: "3.0",
+        sampled_at: "Today, 14:42",
+      },
+      {
+        brand: "Hatzi Hinam",
+        branch: "Beer Sheva - Main street 10",
+        price: "3.1",
+        sampled_at: "Today, 14:44",
+      },
+      {
+        brand: "Mega",
+        branch: "Haifa - Sheset yamim 20",
+        price: "4.1",
+        sampled_at: "Today, 14:35",
+      },
+    ],
   }
 };
--- front/testing/src/views/ProductView.vue
+++ front/testing/src/views/ProductView.vue
@@ -1,31 +1,132 @@
 <template>
-  <div class="row">
+  <div class="row product-page">
     <div class="col-12">
 
       <PagesHeader></PagesHeader>
-      <BreadCrumbs></BreadCrumbs>
 
-      {{ product.name }}
+      <section class="row" v-if="Object.keys(product).length === 0">
+        <div class="col-12 text-center">
+          <i class="fal fa-spinner fa-spin h1 "></i>
+        </div>
+      </section>
+
+      <section class="row" v-if="Object.keys(product).length !== 0">
+
+        <div class="col-2">
+          <img v-bind:src="product.image" class="img-fluid" />
+        </div>
+
+        <div class="col-10">
+
+          <div class="row">
+            <div class="col-9"><h1>{{ product.name }}</h1></div>
+            <div class="col-3 text-right"><span class="h2">Average price: {{product.average_price}}</span></div>
+          </div>
+
+          <hr />
+
+          <div class="row">
+            <div class="col-12">
+              <p class="h3"><i class="fas fa-arrow-up text-danger"></i> Max price {{product.hi_price.amount}} from {{product.hi_price.location}}</p>
+              <p class="h3"><i class="fas fa-arrow-down text-success"></i> Low price {{product.low_price.amount}} from {{product.low_price.location}}</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="row samples" v-if="product.samples !== null">
+        <div class="col-12">
+          <h2>Recent samples from other places</h2>
+
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">brand</th>
+                <th scope="col">Branch</th>
+                <th scope="col">Price</th>
+                <th scope="col">Sampled at</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="sample in product.samples">
+                <td>{{sample.brand}}</td>
+                <td>{{sample.branch}}</td>
+                <td>{{sample.price}}</td>
+                <td>{{sample.sampled_at}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+
+      <section class="row averages-explorer">
+        <div class="col-12">
+          <div class="row">
+            <div class="col-8">
+              <h3>Average price over the years</h3>
+            </div>
+
+            <div class="col-4">
+              asad
+            </div>
+          </div>
+
+          <hr />
+        </div>
+      </section>
+
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import products from "@/mockups/products.js";
 import PagesHeader from "@/components/PagesHeader";
 import BreadCrumbs from "@/components/BreadCrumbs";
+import ProductsService from "@/services/ProductsService";
+
 
 @Component({
   components: {PagesHeader, BreadCrumbs}
 })
 export default class ProductView extends Vue {
+
+  public ProductsService: ProductsService;
+
+  public created() {
+    this.ProductsService = new ProductsService();
+  }
+
+  public async mounted() {
+    this.product = await this.ProductsService.getProductById(this.$route.params.id);
+  }
+
   public data() {
     return {
-      product: products[this.$route.params.id]
+      product: {}
     };
   }
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+  .product-page {
+    text-align: left;
+
+    .pages-header {
+      margin-bottom: 1em;
+    }
+
+    .samples {
+      padding-top: 2em;
+
+      table {
+        margin-top: 1.5em;
+      }
+    }
+
+    .averages-explorer {
+      padding-top: 2em;
+    }
+  }
+</style>
